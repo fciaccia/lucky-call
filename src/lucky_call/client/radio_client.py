7d7bd2bab2929284a9c7db2d21ac1ccca5e71019
Francesco Ciaccia <fra.ciaccia@gmail.com>
2020-10-01 17:41:11 +0200

    remove external class to manager pub/sub queues and moved queues management to an mp.Manager in main

--- src/lucky_call/client/radio_client.py
+++ src/lucky_call/client/radio_client.py
@@ -9,13 +9,9 @@ from lucky_call.common import KEYWORD, MAX_NUM
 
 class RadioClient(mp.Process):
 
-    def __init__(self, pub_queue, req_queue):
+    def __init__(self, sub_queue, req_queue):
         mp.Process.__init__(self)
-        print("client ",pub_queue)
-        print("client ",pub_queue._queues)
-        self.sub_queue = pub_queue.register()
-        print("client ",pub_queue._queues)
-        print("client ",self.sub_queue)
+        self.sub_queue = sub_queue
         self.req_queue = req_queue
 
     def run(self):
@@ -28,7 +24,9 @@ class RadioClient(mp.Process):
                     break
                 if word == KEYWORD:
                     # Simulate random radio delay by waiting up to 1s
-                    sleep(randrange(0,999)/1000)
+                    random_delay = randrange(0,999)/1000
+                    print("Client %d sleeping random delay %.3f"%(os.getpid(), random_delay))
+                    sleep(random_delay)
 
                     # Generate request with random number
                     self.req_queue.put(
@@ -43,4 +41,6 @@ class RadioClient(mp.Process):
                     continue
             except Exception as e:
                 print(e)
+                sys.exit(0)
+            except KeyboardInterrupt:
                 sys.exit(0)
--- src/lucky_call/common.py
+++ src/lucky_call/common.py
@@ -11,31 +11,3 @@ MAX_NUM = 999
 class Defaults(Enum):
     DEFAULT_DB_FILENAME = 'local.db'
     DEFAULT_NUM_CLIENTS = 100
-
-
-class PublishQueue(object):
-    ''' From https://stackoverflow.com/questions/31267366/how-can-i-implement-a-pub-sub-pattern-using-multiprocessing '''
-    def __init__(self):
-        self._queues = []
-        self._creator_pid = os.getpid()
-
-    def __getstate__(self):
-        self_dict = self.__dict__
-        self_dict['_queues'] = []
-        return self_dict
-
-    def __setstate__(self, state):
-        self.__dict__.update(state)
-
-    def register(self):
-        q = mp.Queue()
-        print("publish queue register", q)
-        self._queues.append(q)
-        return q
-
-    def publish(self, val):
-        print("publish queue publish", self._queues)
-        for q in self._queues:
-            print("Publishing message in queue ", q)
-            q.put(val)
-
