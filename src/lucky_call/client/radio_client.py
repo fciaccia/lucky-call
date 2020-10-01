a67baba06ff93e2b18c56da1b96f95eee33e4e84
Francesco Ciaccia <fra.ciaccia@gmail.com>
2020-10-01 18:14:20 +0200

    pep8

--- src/lucky_call/client/radio_client.py
+++ src/lucky_call/client/radio_client.py
@@ -7,6 +7,7 @@ from time import sleep
 
 from lucky_call.common import KEYWORD, MAX_NUM
 
+
 class RadioClient(mp.Process):
 
     def __init__(self, sub_queue, req_queue):
@@ -24,23 +25,24 @@ class RadioClient(mp.Process):
                     break
                 if word == KEYWORD:
                     # Simulate random radio delay by waiting up to 1s
-                    random_delay = randrange(0,999)/1000
-                    print("Client %d sleeping random delay %.3f"%(os.getpid(), random_delay))
+                    random_delay = randrange(0, 999)/1000
+                    print("Client %d sleeping random delay %.3f" %
+                          (os.getpid(), random_delay))
                     sleep(random_delay)
 
                     # Generate request with random number
                     self.req_queue.put(
                         {
                             'pid': os.getpid(),
-                            'message': KEYWORD+str(randrange(0,MAX_NUM))
+                            'message': KEYWORD+str(randrange(0, MAX_NUM))
                         }
                     )
                     # Terminate client after sending request
                     break
                 else:
                     continue
+            except KeyboardInterrupt:
+                sys.exit(0)
             except Exception as e:
                 print(e)
-                sys.exit(0)
-            except KeyboardInterrupt:
                 sys.exit(0)
